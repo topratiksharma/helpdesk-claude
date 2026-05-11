f8a38f5a6fee74117bd30fd2540dc6815d469424
Pratik Sharma <pratiksharma@live.in>
2026-05-11 11:20:53 +0200

    Generated files Ignored.

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
